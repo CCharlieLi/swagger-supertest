0a623856db9f94acfd91ec04e8b14a1cf8468a56
CCharlieLi <ccharlieli@live.com>
2019-12-05 14:02:13 +0800

    chore: add request body for put and patch

--- test/swagger.test.js
+++ test/swagger.test.js
@@ -34,6 +34,36 @@ describe('Generate swagger data', () => {
       });
   });
 
+  it('should generate swagger file for PUT', done => {
+    const app = express();
+    app.put('/api/orders/realm1/user', (req, res) => {
+      res.send({
+        data: [
+          {
+            id: 'orderId',
+            userName: 'charlie',
+            price: 123.45
+          }
+        ]
+      });
+    });
+
+    request(app)
+      .put('/api/orders/realm1/user?limit=1&offset=2')
+      .send({
+        data: {
+          userId: 'charlie-123'
+        }
+      })
+      .set('x-transaction-id', '111')
+      .set('x-user-name', '222')
+      .set('swagger-supertest', './doc-test')
+      .end((err, res) => {
+        res.body.data.length.should.be.equal(1);
+        done();
+      });
+  });
+
   it('should generate swagger file for GET', done => {
     const app = express();
 
